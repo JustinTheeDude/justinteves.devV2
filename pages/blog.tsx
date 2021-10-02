9a36ea626c9f1088d7a23067403debae76bfc13c
JustinTheeDude <JustinTheeDude@gmail.com>
2021-10-02 13:33:43 -0700

    asdf

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -3,7 +3,7 @@ import { Heading } from '@chakra-ui/react';
 
 const Blog: NextPage = () => {
 	return (
-		<Heading textAlign="center">BLOG COMING SOON</Heading>
+		<Heading textAlign="center">BLOG COMIN SOON</Heading>
 	)
 }
 
